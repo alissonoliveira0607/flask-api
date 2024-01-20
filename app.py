505181c2d57fc60b49f982854cd6117ef08832df
Alisson Oliveira <alissonoliveira0607@gmail.com>
2024-01-20 16:57:43 -0300

    Implemented route: GET purchase orders by items and by id and POST purchase orders

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -28,11 +28,50 @@ purchase_orders = [
     }
 ]
 
-
+# Return all purchase_orders
 @app.route('/purchase_orders', methods=['GET'])
 def get_purchase_orders():
     return jsonify(purchase_orders)
 
+# Return purchase_order from id
+@app.route('/purchase_orders/<int:id>', methods=['GET'])
+def get_purchase_orders_by_id(id):
+    try:
+        for orders in purchase_orders:
+            if orders['id'] == id:
+                return jsonify(orders)
+            else:
+                return jsonify({'message': f'Purchase order id {id} Not found'})
+    except Exception as e:
+        print(e)
+
+# Create purchase order
+@app.route('/purchase_orders', methods=['POST'])
+def post_purchase_orders():
+    request_data = request.get_json() # resgatando os dados do request
+    for order in purchase_orders:
+        if order['id'] == request_data['id']:
+            return jsonify({'message': f'Purchase order id {request_data["id"]} already exists'}), 400
+    purchase_order = {
+        'id': request_data['id'],
+        'description': request_data['description'],
+        'items': []
+        }
+    purchase_orders.append(purchase_order)
+    return jsonify(purchase_orders), 201
+
+
+
+@app.route('/purchase_orders/<int:id>/items')
+def get_items_purchase_orders(id):
+    try:
+        for order in purchase_orders:
+            if order['id'] == id:
+                return jsonify(order['items']), 200
+            
+        return jsonify({'message': f'Purchase order id {id} Not found'}), 400
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
